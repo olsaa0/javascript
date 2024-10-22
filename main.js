43c9d56b2aa607b231ec29cb66381b7a8eb9bb1f
olsaa0 <olsaolsa30@gmail.com>
2024-10-22 16:18:51 +0200

    kushte

---
+++ main.js
@@ -0,0 +1,12 @@
+age = 15;
+qytet = "Prishtina";
+
+if(age>=18 && qytet == "Prishtina"){
+    console.log("You can vote.")
+}
+else{
+    console.log("You cannot vote.")
+}
+
+var cars=['audi', 'mercides', 'bmw', 'lambo', 'idk', 'rolls royce'];
+if(cars[5]=='bmw')
